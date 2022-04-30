70dff9f6619ed001da1f710933d5a899052b4ab1
Dawid Lower <lowdaw@gmail.com>
2022-04-30 14:12:52 +0200

    Ogarnianie IFów

--- skrypt1/skrypt.py
+++ skrypt1/skrypt.py
@@ -1,1 +1,18 @@
-print("Hello World")
+#print("Hello World")
+
+#IFY
+#print("Podaj swój wzrost")
+#
+#input1=input()
+#wzrost=int(input1)
+#
+#if(wzrost>180):
+#    print('jesteś duży')
+#elif(wzrost>170):
+#    print('nie jesteś duży')
+#    print("brakuje Ci", 190-wzrost, "cm, aby być dużym")
+#elif(wzrost>160):
+#    print('jesteś karłem')
+#    print("brakuje Ci", 190-wzrost, "cm, aby być dużym")
+#else:
+#    print('to już przesada xD')
