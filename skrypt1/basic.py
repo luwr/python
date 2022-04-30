f8fad5f0b56b2cd5e10e1d4c65b1062a7e3cec1d
Dawid Lower <lowdaw@gmail.com>
2022-04-30 15:16:33 +0200

    Porzadki w plikach

---
+++ skrypt1/basic.py
@@ -0,0 +1,42 @@
+#print("Hello World")
+
+#IFY
+#print("Podaj swój wzrost")
+#
+#input1=input()
+#wzrost=int(input1)
+#
+#if(wzrost>180):
+#    print('jesteś duży')
+#elif(wzrost>170):
+#    print('nie jesteś duży')
+#    print("brakuje Ci", 190-wzrost, "cm, aby być dużym")
+#elif(wzrost>160):
+#    print('jesteś karłem')
+#    print("brakuje Ci", 190-wzrost, "cm, aby być dużym")
+#else:
+#    print('to już przesada xD')
+
+if ("X" in "SpaceX"):
+    print("ten ciąg zawiera X!")
+
+#WHILE
+#x=1
+#while(x<=10):
+#    print(x)
+#    x+=1
+
+
+#FOR
+#for i in range(0,11,2):
+#    print(i)
+
+#FOR ZAGNIEŻDŻONY
+#for x in range(1,6):
+#    for y in range(1,10):
+#        print('x+{}, y={}'.format(x,y))
+
+#for x in range(1,101):
+#    if(x%17.5==0):
+#        print("szukana liczba to {}".format(x))
+#        break
---
+++ skrypt1/stringi.py
@@ -0,0 +1,22 @@
+
+#MNOŻENIE STRINGÓW
+#kriszna="rama "*5
+#print(kriszna)
+
+#COUNTER PO LITERACH
+#nazwa="litery"
+#for n in nazwa:
+#    print(n)
+
+#SPLIT
+#napis="MINISTERSTWO DO SPRAW NIEZBYT ISTOTNYCH SPRAW"
+#print(napis.split())
+
+
+#CIĘCIE STRINGÓW
+lancuch="123456789"
+print(lancuch[2]) #3 znak
+print(lancuch[-2]) #2 znak od konca
+print(lancuch[2:5]) #od 3 do 5 znaku
+print(lancuch[:5]) #od 3 do 5 znaku
+print(lancuch[0:6:2])
