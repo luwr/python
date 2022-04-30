063143d62bf1f2f7ce760779903f28f2d49ea00e
Dawid Lower <lowdaw@gmail.com>
2022-04-30 14:23:13 +0200

    Petle

--- skrypt1/skrypt.py
+++ skrypt1/skrypt.py
@@ -16,3 +16,25 @@
 #    print("brakuje Ci", 190-wzrost, "cm, aby być dużym")
 #else:
 #    print('to już przesada xD')
+
+
+#WHILE
+#x=1
+#while(x<=10):
+#    print(x)
+#    x+=1
+
+
+#FOR
+#for i in range(0,11,2):
+#    print(i)
+
+#FOR ZAGNIEŻDŻONY
+#for x in range(1,6):
+#    for y in range(1,10):
+#        print('x+{}, y={}'.format(x,y))
+
+for x in range(1,101):
+    if(x%17.5==0):
+        print("szukana liczba to {}".format(x))
+        break
